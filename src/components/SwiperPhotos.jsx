0bb8abfe17b6301ff5b26576652ab738ccedbfc9
Joanseco <joanseco5@gmail.com>
2026-01-23 09:51:42 -0500

    añadi 3 fotos

--- src/components/SwiperPhotos.jsx
+++ src/components/SwiperPhotos.jsx
@@ -9,15 +9,18 @@ import 'swiper/css/pagination';
 
 // Array de datos con las URLs de las imágenes y una descripción
 const photoSlides = [
-    { id: 1, src: '/assets/swiper-photos/1.jpg', alt: 'Cacao de Serrato en Francia' },
-    { id: 2, src: '/assets/swiper-photos/2.jpg', alt: 'Cacao de Serrato en Francia' },
-    { id: 3, src: '/assets/swiper-photos/3.jpg', alt: 'Cacao de Serrato en Francia' },
+    { id: 1, src: '/assets/swiper-photos/1.jpg', alt: 'Cacao de Serrato en Cartagena' },
+    { id: 2, src: '/assets/swiper-photos/2.jpeg', alt: 'Cacao de Serrato en Francia' },
+    { id: 3, src: '/assets/swiper-photos/3.jpg', alt: 'Cacao de Serrato en Cartagena' },
     { id: 4, src: '/assets/swiper-photos/4.jpg', alt: 'Cacao de Serrato en Francia' },
     { id: 5, src: '/assets/swiper-photos/5.jpg', alt: 'Cacao de Serrato en Francia' },
     { id: 6, src: '/assets/swiper-photos/6.jpg', alt: 'Cacao de Serrato en Francia' },
-    { id: 7, src: '/assets/swiper-photos/7.jpg', alt: 'Cacao de Serrato en Francia' },
+    { id: 7, src: '/assets/swiper-photos/7.jpeg', alt: 'Cacao de Serrato en Francia' },
     { id: 8, src: '/assets/swiper-photos/8.jpg', alt: 'Cacao de Serrato en Francia' },
     { id: 9, src: '/assets/swiper-photos/9.jpg', alt: 'Cacao de Serrato en Francia' },
+    { id: 10, src: '/assets/swiper-photos/10.jpg', alt: 'Cacao de Serrato en Francia' },
+    { id: 11, src: '/assets/swiper-photos/11.jpg', alt: 'Cacao de Serrato en Francia' },
+    { id: 12, src: '/assets/swiper-photos/12.jpeg', alt: 'Cacao de Serrato en Cartagena' },
 ];
 
 const SwiperPhotos = () => {
@@ -30,7 +33,7 @@ const SwiperPhotos = () => {
           viewport={{ once: true }}
         >
         <div className="max-w-2xl mx-auto mt-10 mb-10 p-8 bg-gray-100 rounded-xl shadow-2xl text-center">
-            <h2 className="text-4xl mb-4 text-center text-amber-800">Cacao de Serrato por el mundo</h2>
+            <h2 className="text-4xl lg:text-4xl font-bold text-amber-900 mb-8">Cacao de Serrato por el mundo</h2>
 
             {/* 4. Componente SWIPER */}
             <Swiper
